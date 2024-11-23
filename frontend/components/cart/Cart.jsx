3d5d9494b8493b0a7b087b161a74ad3a4d9673da
YuejieXuUCM <xuyuejie45@gmail.com>
2024-11-23 17:29:04 +0100

    Estilos

--- frontend/components/cart/Cart.jsx
+++ frontend/components/cart/Cart.jsx
@@ -119,7 +119,7 @@ const styles = StyleSheet.create({
     },
     container: {
         width: "93%",
-        height: "340pt",
+        maxHeight: "85vh",
         padding: 40,
         marginTop: 40,
         margin: "auto",
